a21fb8b9acba6cea14cae804ba4a26693e0b6d67
Lohana Torres <lt.leopoldo@gmail.com>
2021-02-27 17:24:25 -0300

    feat: handle leveling

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,9 +1,24 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export default function ChallengeBox() {
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(
+    ChallengesContext
+  );
+
+  const { stopCountdown } = useContext(CountdownContext);
+
+  function handleChallengeSucceeded() {
+    completeChallenge();
+    stopCountdown();
+  }
+
+  function handleChallengeFailed() {
+    resetChallenge();
+    stopCountdown();
+  }
 
   return (
     <div className={styles.challengeBoxContainer}>
@@ -21,11 +36,15 @@ export default function ChallengeBox() {
             <button
               type="button"
               className={styles.challengeFailedButton}
-              onClick={resetChallenge}
+              onClick={handleChallengeFailed}
             >
               Falhei
             </button>
-            <button type="button" className={styles.challengeSucceededButton}>
+            <button
+              type="button"
+              className={styles.challengeSucceededButton}
+              onClick={handleChallengeSucceeded}
+            >
               Completei
             </button>
           </footer>
