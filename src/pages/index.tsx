a14b961d43926c5551c68cf0fb60775acbbc00c1
Lohana Torres <lt.leopoldo@gmail.com>
2021-02-27 17:25:42 -0300

    feat: use countdown provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import ChallengeBox from '../components/ChallengeBox';
 
 import styles from '../styles/pages/Home.module.css';
 
+import { CountdownProvider } from '../contexts/CountdownContext';
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -17,16 +19,18 @@ export default function Home() {
 
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   );
 }
